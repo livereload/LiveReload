cf57f90698821750ac8836e070c89ca9b64c7467
Andrey Tarantsov <andreyvit@gmail.com>
2011-05-15 08:00:23 +0700

    Remove the main window

--- LiveReload/Classes/Application/LiveReloadAppDelegate.h
+++ LiveReload/Classes/Application/LiveReloadAppDelegate.h
@@ -4,10 +4,8 @@
 @class StatusItemController;
 
 @interface LiveReloadAppDelegate : NSObject <NSApplicationDelegate> {
-    NSWindow *window;
 }
 
-@property (assign) IBOutlet NSWindow *window;
 @property(nonatomic, retain, readonly) StatusItemController *statusItemController;
 
 @end
--- LiveReload/Classes/Application/LiveReloadAppDelegate.m
+++ LiveReload/Classes/Application/LiveReloadAppDelegate.m
@@ -13,12 +13,10 @@
 
 @implementation LiveReloadAppDelegate
 
-@synthesize window;
 @synthesize statusItemController=_statusItemController;
 
 // just to make XDry happy; won't ever be deallocated
 - (void)dealloc {
-    [window release], window = nil;
     [super dealloc];
 }
 
