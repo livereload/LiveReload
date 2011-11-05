7fc7542b4c96c331c174a323163b98f90c47ada7
Andrey Tarantsov <andreyvit@gmail.com>
2011-11-06 00:56:08 +0700

    Redesign WIP

--- LiveReload/Classes/Controllers/NewMainWindowController.h
+++ LiveReload/Classes/Controllers/NewMainWindowController.h
@@ -1,8 +1,19 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface NewMainWindowController : NSWindowController
+@interface NewMainWindowController : NSWindowController {
+    NSObject              *_projectsItem;
+    NSImage               *_folderImage;
+}
 
 @property (nonatomic, readonly) NSArray *projects;
+
+@property (assign) IBOutlet NSOutlineView *projectOutlineView;
+
+@property (assign) IBOutlet NSTextField *pathTextField;
+
+@property (assign) IBOutlet NSButton *compilerEnabledCheckBox;
+
+@property (assign) IBOutlet NSButton *postProcessingEnabledCheckBox;
 
 @end
--- LiveReload/Classes/Controllers/NewMainWindowController.m
+++ LiveReload/Classes/Controllers/NewMainWindowController.m
@@ -1,23 +1,135 @@
 
 #import "NewMainWindowController.h"
 
+#import "ImageAndTextCell.h"
+
 #import "Workspace.h"
+#import "Project.h"
 
 @implementation NewMainWindowController
+@synthesize projectOutlineView;
+@synthesize pathTextField = _pathTextField;
+@synthesize compilerEnabledCheckBox = _compilerEnabledCheckBox;
+@synthesize postProcessingEnabledCheckBox = _postProcessingEnabledCheckBox;
 
 - (id)init {
     self = [super initWithWindowNibName:@"NewMainWindow"];
     if (self) {
+        _projectsItem = [[NSObject alloc] init];
+
+        _folderImage = [[[NSWorkspace sharedWorkspace] iconForFileType:NSFileTypeForHFSTypeCode(kGenericFolderIcon)] retain];
+        [_folderImage setSize:NSMakeSize(16,16)];
     }
     return self;
 }
 
 - (void)windowDidLoad {
     [super windowDidLoad];
+    NSString *version = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    self.window.title = [NSString stringWithFormat:@"LiveReload %@", version];
+
+    NSTableColumn *tableColumn = [projectOutlineView tableColumnWithIdentifier:@"Name"];
+    ImageAndTextCell *imageAndTextCell = [[[ImageAndTextCell alloc] init] autorelease];
+    [imageAndTextCell setEditable:YES];
+    [tableColumn setDataCell:imageAndTextCell];
+
+    // scroll to the top in case the outline contents is very long
+    [[[projectOutlineView enclosingScrollView] verticalScroller] setFloatValue:0.0];
+    [[[projectOutlineView enclosingScrollView] contentView] scrollToPoint:NSMakePoint(0,0)];
+    [projectOutlineView setSelectionHighlightStyle:NSTableViewSelectionHighlightStyleSourceList];
+
+    [projectOutlineView expandItem:_projectsItem];
 }
 
 - (NSArray *)projects {
     return [Workspace sharedWorkspace].sortedProjects;
+}
+
+
+#pragma mark - NSOutlineView data source and delegate
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView shouldSelectItem:(id)item {
+    return (item != nil && item != _projectsItem);
+}
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView isGroupItem:(id)item {
+    return item == _projectsItem;
+}
+
+- (id)outlineView:(NSOutlineView *)outlineView child:(NSInteger)index ofItem:(id)item {
+    if (item == nil)
+        return _projectsItem;
+    if (item == _projectsItem)
+        return [self.projects objectAtIndex:index];
+    assert(0);
+}
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item {
+    if (item == nil)
+        return YES;
+    if (item == _projectsItem)
+        return YES;
+    return NO;
+}
+
+- (NSInteger)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(id)item {
+    if (item == nil)
+        return 1;
+    if (item == _projectsItem)
+        return [self.projects count];
+    return 0;
+}
+
+- (id)outlineView:(NSOutlineView *)outlineView objectValueForTableColumn:(NSTableColumn *)tableColumn byItem:(id)item {
+    NSParameterAssert(item != nil);
+    if (item == _projectsItem)
+        return @"MONITORED FOLDERS";
+    return [((Project *)item).path lastPathComponent];
+}
+
+- (void)outlineView:(NSOutlineView *)outlineView willDisplayCell:(id)cell forTableColumn:(NSTableColumn *)tableColumn item:(id)item {
+    ImageAndTextCell *theCell = cell;
+    if (item == nil || item == _projectsItem) {
+        theCell.image = nil;
+    } else {
+        theCell.image = _folderImage;
+    }
+}
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView shouldShowOutlineCellForItem:(id)item {
+    return NO;
+}
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView shouldCollapseItem:(id)item {
+    return NO;
+}
+
+- (NSString *)outlineView:(NSOutlineView *)outlineView toolTipForCell:(NSCell *)cell rect:(NSRectPointer)rect tableColumn:(NSTableColumn *)tableColumn item:(id)item mouseLocation:(NSPoint)mouseLocation {
+    if (item == nil || item == _projectsItem) {
+        return nil;
+    } else {
+        Project *project = item;
+        return project.displayPath;
+    }
+}
+
+//- (NSDragOperation)outlineView:(NSOutlineView *)outlineView validateDrop:(id <NSDraggingInfo>)info proposedItem:(id)item proposedChildIndex:(NSInteger)index;
+
+//- (BOOL)outlineView:(NSOutlineView *)outlineView acceptDrop:(id <NSDraggingInfo>)info item:(id)item childIndex:(NSInteger)index;
+
+//- (BOOL)selectionShouldChangeInOutlineView:(NSOutlineView *)outlineView;
+
+
+
+#pragma mark - Actions
+
+- (IBAction)showMonitoringOptions:(id)sender {
+}
+
+- (IBAction)showCompilationOptions:(id)sender {
+}
+
+- (IBAction)showPostProcessingOptions:(id)sender {
 }
 
 @end
