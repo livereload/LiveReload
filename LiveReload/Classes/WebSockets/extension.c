e4f1c6506f4cf41fa212a74762256f85a9675916
Andrey Tarantsov <andreyvit@gmail.com>
2011-11-06 00:56:14 +0700

    Whitespace fix

--- LiveReload/Classes/WebSockets/extension.c
+++ LiveReload/Classes/WebSockets/extension.c
@@ -5,18 +5,18 @@
 
 struct libwebsocket_extension libwebsocket_internal_extensions[] = {
 #ifdef LWS_EXT_GOOGLE_MUX
-	{
-		"x-google-mux",
-		lws_extension_callback_x_google_mux,
-		sizeof (struct lws_ext_x_google_mux_conn)
-	},
+    {
+        "x-google-mux",
+        lws_extension_callback_x_google_mux,
+        sizeof (struct lws_ext_x_google_mux_conn)
+    },
 #endif
-	{
-		"deflate-stream",
-		lws_extension_callback_deflate_stream,
-		sizeof (struct lws_ext_deflate_stream_conn)
-	},
-	{ /* terminator */
-		NULL, NULL, 0
-	}
+    {
+        "deflate-stream",
+        lws_extension_callback_deflate_stream,
+        sizeof (struct lws_ext_deflate_stream_conn)
+    },
+    { /* terminator */
+        NULL, NULL, 0
+    }
 };
--- LiveReload/Classes/WebSockets/private-libwebsockets.h
+++ LiveReload/Classes/WebSockets/private-libwebsockets.h
@@ -82,8 +82,8 @@ static inline
 #endif
 void debug(const char *format, ...)
 {
-	va_list ap;
-	va_start(ap, format); vfprintf(stderr, format, ap); va_end(ap);
+    va_list ap;
+    va_start(ap, format); vfprintf(stderr, format, ap); va_end(ap);
 }
 #else
 #ifdef WIN32
@@ -122,142 +122,142 @@ void debug(const char *format, ...)
 #define SYSTEM_RANDOM_FILEPATH "/dev/urandom"
 
 enum lws_websocket_opcodes_04 {
-	LWS_WS_OPCODE_04__CONTINUATION = 0,
-	LWS_WS_OPCODE_04__CLOSE = 1,
-	LWS_WS_OPCODE_04__PING = 2,
-	LWS_WS_OPCODE_04__PONG = 3,
-	LWS_WS_OPCODE_04__TEXT_FRAME = 4,
-	LWS_WS_OPCODE_04__BINARY_FRAME = 5,
+    LWS_WS_OPCODE_04__CONTINUATION = 0,
+    LWS_WS_OPCODE_04__CLOSE = 1,
+    LWS_WS_OPCODE_04__PING = 2,
+    LWS_WS_OPCODE_04__PONG = 3,
+    LWS_WS_OPCODE_04__TEXT_FRAME = 4,
+    LWS_WS_OPCODE_04__BINARY_FRAME = 5,
 
-	LWS_WS_OPCODE_04__RESERVED_6 = 6,
-	LWS_WS_OPCODE_04__RESERVED_7 = 7,
-	LWS_WS_OPCODE_04__RESERVED_8 = 8,
-	LWS_WS_OPCODE_04__RESERVED_9 = 9,
-	LWS_WS_OPCODE_04__RESERVED_A = 0xa,
-	LWS_WS_OPCODE_04__RESERVED_B = 0xb,
-	LWS_WS_OPCODE_04__RESERVED_C = 0xc,
-	LWS_WS_OPCODE_04__RESERVED_D = 0xd,
-	LWS_WS_OPCODE_04__RESERVED_E = 0xe,
-	LWS_WS_OPCODE_04__RESERVED_F = 0xf,
+    LWS_WS_OPCODE_04__RESERVED_6 = 6,
+    LWS_WS_OPCODE_04__RESERVED_7 = 7,
+    LWS_WS_OPCODE_04__RESERVED_8 = 8,
+    LWS_WS_OPCODE_04__RESERVED_9 = 9,
+    LWS_WS_OPCODE_04__RESERVED_A = 0xa,
+    LWS_WS_OPCODE_04__RESERVED_B = 0xb,
+    LWS_WS_OPCODE_04__RESERVED_C = 0xc,
+    LWS_WS_OPCODE_04__RESERVED_D = 0xd,
+    LWS_WS_OPCODE_04__RESERVED_E = 0xe,
+    LWS_WS_OPCODE_04__RESERVED_F = 0xf,
 };
 
 enum lws_websocket_opcodes_07 {
-	LWS_WS_OPCODE_07__CONTINUATION = 0,
-	LWS_WS_OPCODE_07__TEXT_FRAME = 1,
-	LWS_WS_OPCODE_07__BINARY_FRAME = 2,
+    LWS_WS_OPCODE_07__CONTINUATION = 0,
+    LWS_WS_OPCODE_07__TEXT_FRAME = 1,
+    LWS_WS_OPCODE_07__BINARY_FRAME = 2,
 
-	LWS_WS_OPCODE_07__NOSPEC__MUX = 7,
+    LWS_WS_OPCODE_07__NOSPEC__MUX = 7,
 
-	/* control extensions 8+ */
+    /* control extensions 8+ */
 
-	LWS_WS_OPCODE_07__CLOSE = 8,
-	LWS_WS_OPCODE_07__PING = 9,
-	LWS_WS_OPCODE_07__PONG = 0xa,
+    LWS_WS_OPCODE_07__CLOSE = 8,
+    LWS_WS_OPCODE_07__PING = 9,
+    LWS_WS_OPCODE_07__PONG = 0xa,
 };
 
 
 enum lws_connection_states {
-	WSI_STATE_HTTP,
-	WSI_STATE_HTTP_HEADERS,
-	WSI_STATE_DEAD_SOCKET,
-	WSI_STATE_ESTABLISHED,
-	WSI_STATE_CLIENT_UNCONNECTED,
-	WSI_STATE_RETURNED_CLOSE_ALREADY,
-	WSI_STATE_AWAITING_CLOSE_ACK,
+    WSI_STATE_HTTP,
+    WSI_STATE_HTTP_HEADERS,
+    WSI_STATE_DEAD_SOCKET,
+    WSI_STATE_ESTABLISHED,
+    WSI_STATE_CLIENT_UNCONNECTED,
+    WSI_STATE_RETURNED_CLOSE_ALREADY,
+    WSI_STATE_AWAITING_CLOSE_ACK,
 };
 
 enum lws_rx_parse_state {
-	LWS_RXPS_NEW,
+    LWS_RXPS_NEW,
 
-	LWS_RXPS_SEEN_76_FF,
-	LWS_RXPS_PULLING_76_LENGTH,
-	LWS_RXPS_EAT_UNTIL_76_FF,
+    LWS_RXPS_SEEN_76_FF,
+    LWS_RXPS_PULLING_76_LENGTH,
+    LWS_RXPS_EAT_UNTIL_76_FF,
 
-	LWS_RXPS_04_MASK_NONCE_1,
-	LWS_RXPS_04_MASK_NONCE_2,
-	LWS_RXPS_04_MASK_NONCE_3,
+    LWS_RXPS_04_MASK_NONCE_1,
+    LWS_RXPS_04_MASK_NONCE_2,
+    LWS_RXPS_04_MASK_NONCE_3,
 
-	LWS_RXPS_04_FRAME_HDR_1,
-	LWS_RXPS_04_FRAME_HDR_LEN,
-	LWS_RXPS_04_FRAME_HDR_LEN16_2,
-	LWS_RXPS_04_FRAME_HDR_LEN16_1,
-	LWS_RXPS_04_FRAME_HDR_LEN64_8,
-	LWS_RXPS_04_FRAME_HDR_LEN64_7,
-	LWS_RXPS_04_FRAME_HDR_LEN64_6,
-	LWS_RXPS_04_FRAME_HDR_LEN64_5,
-	LWS_RXPS_04_FRAME_HDR_LEN64_4,
-	LWS_RXPS_04_FRAME_HDR_LEN64_3,
-	LWS_RXPS_04_FRAME_HDR_LEN64_2,
-	LWS_RXPS_04_FRAME_HDR_LEN64_1,
+    LWS_RXPS_04_FRAME_HDR_1,
+    LWS_RXPS_04_FRAME_HDR_LEN,
+    LWS_RXPS_04_FRAME_HDR_LEN16_2,
+    LWS_RXPS_04_FRAME_HDR_LEN16_1,
+    LWS_RXPS_04_FRAME_HDR_LEN64_8,
+    LWS_RXPS_04_FRAME_HDR_LEN64_7,
+    LWS_RXPS_04_FRAME_HDR_LEN64_6,
+    LWS_RXPS_04_FRAME_HDR_LEN64_5,
+    LWS_RXPS_04_FRAME_HDR_LEN64_4,
+    LWS_RXPS_04_FRAME_HDR_LEN64_3,
+    LWS_RXPS_04_FRAME_HDR_LEN64_2,
+    LWS_RXPS_04_FRAME_HDR_LEN64_1,
 
-	LWS_RXPS_07_COLLECT_FRAME_KEY_1,
-	LWS_RXPS_07_COLLECT_FRAME_KEY_2,
-	LWS_RXPS_07_COLLECT_FRAME_KEY_3,
-	LWS_RXPS_07_COLLECT_FRAME_KEY_4,
+    LWS_RXPS_07_COLLECT_FRAME_KEY_1,
+    LWS_RXPS_07_COLLECT_FRAME_KEY_2,
+    LWS_RXPS_07_COLLECT_FRAME_KEY_3,
+    LWS_RXPS_07_COLLECT_FRAME_KEY_4,
 
-	LWS_RXPS_PAYLOAD_UNTIL_LENGTH_EXHAUSTED
+    LWS_RXPS_PAYLOAD_UNTIL_LENGTH_EXHAUSTED
 };
 
 
 enum connection_mode {
-	LWS_CONNMODE_WS_SERVING,
-	LWS_CONNMODE_WS_CLIENT,
+    LWS_CONNMODE_WS_SERVING,
+    LWS_CONNMODE_WS_CLIENT,
 
-	/* transient modes */
-	LWS_CONNMODE_WS_CLIENT_WAITING_PROXY_REPLY,
-	LWS_CONNMODE_WS_CLIENT_ISSUE_HANDSHAKE,
-	LWS_CONNMODE_WS_CLIENT_WAITING_SERVER_REPLY,
-	LWS_CONNMODE_WS_CLIENT_WAITING_EXTENSION_CONNECT,
-	LWS_CONNMODE_WS_CLIENT_PENDING_CANDIDATE_CHILD,
+    /* transient modes */
+    LWS_CONNMODE_WS_CLIENT_WAITING_PROXY_REPLY,
+    LWS_CONNMODE_WS_CLIENT_ISSUE_HANDSHAKE,
+    LWS_CONNMODE_WS_CLIENT_WAITING_SERVER_REPLY,
+    LWS_CONNMODE_WS_CLIENT_WAITING_EXTENSION_CONNECT,
+    LWS_CONNMODE_WS_CLIENT_PENDING_CANDIDATE_CHILD,
 
-	/* special internal types */
-	LWS_CONNMODE_SERVER_LISTENER,
-	LWS_CONNMODE_BROADCAST_PROXY_LISTENER,
-	LWS_CONNMODE_BROADCAST_PROXY
+    /* special internal types */
+    LWS_CONNMODE_SERVER_LISTENER,
+    LWS_CONNMODE_BROADCAST_PROXY_LISTENER,
+    LWS_CONNMODE_BROADCAST_PROXY
 };
 
 
 #define LWS_FD_HASH(fd) ((fd ^ (fd >> 8) ^ (fd >> 16)) % FD_HASHTABLE_MODULUS)
 
 struct libwebsocket_fd_hashtable {
-	struct libwebsocket *wsi[MAX_CLIENTS + 1];
-	int length;
+    struct libwebsocket *wsi[MAX_CLIENTS + 1];
+    int length;
 };
 
 struct libwebsocket_protocols;
 
 struct libwebsocket_context {
-	struct libwebsocket_fd_hashtable fd_hashtable[FD_HASHTABLE_MODULUS];
-	struct pollfd fds[MAX_CLIENTS * FD_HASHTABLE_MODULUS + 1];
-	int fds_count;
-	int listen_port;
-	char http_proxy_address[256];
-	char canonical_hostname[1024];
-	unsigned int http_proxy_port;
-	unsigned int options;
-	unsigned long last_timeout_check_s;
+    struct libwebsocket_fd_hashtable fd_hashtable[FD_HASHTABLE_MODULUS];
+    struct pollfd fds[MAX_CLIENTS * FD_HASHTABLE_MODULUS + 1];
+    int fds_count;
+    int listen_port;
+    char http_proxy_address[256];
+    char canonical_hostname[1024];
+    unsigned int http_proxy_port;
+    unsigned int options;
+    unsigned long last_timeout_check_s;
 
-	int fd_random;
+    int fd_random;
 
 #ifdef LWS_OPENSSL_SUPPORT
-	int use_ssl;
-	SSL_CTX *ssl_ctx;
-	SSL_CTX *ssl_client_ctx;
+    int use_ssl;
+    SSL_CTX *ssl_ctx;
+    SSL_CTX *ssl_client_ctx;
 #endif
-	struct libwebsocket_protocols *protocols;
-	int count_protocols;
-	struct libwebsocket_extension *extensions;
+    struct libwebsocket_protocols *protocols;
+    int count_protocols;
+    struct libwebsocket_extension *extensions;
 };
 
 
 enum pending_timeout {
-	NO_PENDING_TIMEOUT = 0,
-	PENDING_TIMEOUT_AWAITING_PROXY_RESPONSE,
-	PENDING_TIMEOUT_ESTABLISH_WITH_SERVER,
-	PENDING_TIMEOUT_AWAITING_SERVER_RESPONSE,
-	PENDING_TIMEOUT_AWAITING_PING,
-	PENDING_TIMEOUT_CLOSE_ACK,
-	PENDING_TIMEOUT_AWAITING_EXTENSION_CONNECT_RESPONSE,
+    NO_PENDING_TIMEOUT = 0,
+    PENDING_TIMEOUT_AWAITING_PROXY_RESPONSE,
+    PENDING_TIMEOUT_ESTABLISH_WITH_SERVER,
+    PENDING_TIMEOUT_AWAITING_SERVER_RESPONSE,
+    PENDING_TIMEOUT_AWAITING_PING,
+    PENDING_TIMEOUT_CLOSE_ACK,
+    PENDING_TIMEOUT_AWAITING_EXTENSION_CONNECT_RESPONSE,
 };
 
 
@@ -268,74 +268,74 @@ enum pending_timeout {
  */
 
 struct libwebsocket {
-	const struct libwebsocket_protocols *protocol;
-	struct libwebsocket_extension *
-				   active_extensions[LWS_MAX_EXTENSIONS_ACTIVE];
-	void * active_extensions_user[LWS_MAX_EXTENSIONS_ACTIVE];
-	int count_active_extensions;
+    const struct libwebsocket_protocols *protocol;
+    struct libwebsocket_extension *
+                   active_extensions[LWS_MAX_EXTENSIONS_ACTIVE];
+    void * active_extensions_user[LWS_MAX_EXTENSIONS_ACTIVE];
+    int count_active_extensions;
 
-	enum lws_connection_states state;
+    enum lws_connection_states state;
 
-	char name_buffer[LWS_MAX_HEADER_NAME_LENGTH];
-	int name_buffer_pos;
-	int current_alloc_len;
-	enum lws_token_indexes parser_state;
-	struct lws_tokens utf8_token[WSI_TOKEN_COUNT];
-	int ietf_spec_revision;
-	char rx_user_buffer[LWS_SEND_BUFFER_PRE_PADDING + MAX_USER_RX_BUFFER +
-						  LWS_SEND_BUFFER_POST_PADDING];
-	int rx_user_buffer_head;
-	enum libwebsocket_write_protocol rx_frame_type;
-	int protocol_index_for_broadcast_proxy;
-	enum pending_timeout pending_timeout;
-	unsigned long pending_timeout_limit;
+    char name_buffer[LWS_MAX_HEADER_NAME_LENGTH];
+    int name_buffer_pos;
+    int current_alloc_len;
+    enum lws_token_indexes parser_state;
+    struct lws_tokens utf8_token[WSI_TOKEN_COUNT];
+    int ietf_spec_revision;
+    char rx_user_buffer[LWS_SEND_BUFFER_PRE_PADDING + MAX_USER_RX_BUFFER +
+                          LWS_SEND_BUFFER_POST_PADDING];
+    int rx_user_buffer_head;
+    enum libwebsocket_write_protocol rx_frame_type;
+    int protocol_index_for_broadcast_proxy;
+    enum pending_timeout pending_timeout;
+    unsigned long pending_timeout_limit;
 
-	int sock;
+    int sock;
 
-	enum lws_rx_parse_state lws_rx_parse_state;
-	char extension_data_pending;
-	struct libwebsocket *candidate_children_list;
-	struct libwebsocket *extension_handles;
+    enum lws_rx_parse_state lws_rx_parse_state;
+    char extension_data_pending;
+    struct libwebsocket *candidate_children_list;
+    struct libwebsocket *extension_handles;
 
-	/* 04 protocol specific */
+    /* 04 protocol specific */
 
-	char key_b64[150];
-	unsigned char masking_key_04[20];
-	unsigned char frame_masking_nonce_04[4];
-	unsigned char frame_mask_04[20];
-	unsigned char frame_mask_index;
-	size_t rx_packet_length;
-	unsigned char opcode;
-	unsigned char final;
+    char key_b64[150];
+    unsigned char masking_key_04[20];
+    unsigned char frame_masking_nonce_04[4];
+    unsigned char frame_mask_04[20];
+    unsigned char frame_mask_index;
+    size_t rx_packet_length;
+    unsigned char opcode;
+    unsigned char final;
 
-	int pings_vs_pongs;
-	unsigned char (*xor_mask)(struct libwebsocket *, unsigned char);
-	char all_zero_nonce;
+    int pings_vs_pongs;
+    unsigned char (*xor_mask)(struct libwebsocket *, unsigned char);
+    char all_zero_nonce;
 
-	enum lws_close_status close_reason;
+    enum lws_close_status close_reason;
 
-	/* 07 specific */
-	char this_frame_masked;
+    /* 07 specific */
+    char this_frame_masked;
 
-	/* client support */
-	char initial_handshake_hash_base64[30];
-	enum connection_mode mode;
-	char *c_path;
-	char *c_host;
-	char *c_origin;
-	char *c_protocol;
+    /* client support */
+    char initial_handshake_hash_base64[30];
+    enum connection_mode mode;
+    char *c_path;
+    char *c_host;
+    char *c_origin;
+    char *c_protocol;
 
-	char *c_address;
-	int c_port;
+    char *c_address;
+    int c_port;
 
 
 #ifdef LWS_OPENSSL_SUPPORT
-	SSL *ssl;
-	BIO *client_bio;
-	int use_ssl;
+    SSL *ssl;
+    BIO *client_bio;
+    int use_ssl;
 #endif
 
-	void *user_space;
+    void *user_space;
 };
 
 extern int
@@ -346,11 +346,11 @@ libwebsocket_parse(struct libwebsocket *wsi, unsigned char c);
 
 extern int
 libwebsocket_interpret_incoming_packet(struct libwebsocket *wsi,
-						unsigned char *buf, size_t len);
+                        unsigned char *buf, size_t len);
 
 extern int
 libwebsocket_read(struct libwebsocket_context *context, struct libwebsocket *wsi,
-					       unsigned char * buf, size_t len);
+                           unsigned char * buf, size_t len);
 
 extern int
 lws_b64_selftest(void);
@@ -375,7 +375,7 @@ delete_from_fd(struct libwebsocket_context *context, int fd);
 
 extern void
 libwebsocket_set_timeout(struct libwebsocket *wsi,
-					 enum pending_timeout reason, int secs);
+                     enum pending_timeout reason, int secs);
 
 extern int
 lws_issue_raw(struct libwebsocket *wsi, unsigned char *buf, size_t len);
@@ -383,43 +383,43 @@ lws_issue_raw(struct libwebsocket *wsi, unsigned char *buf, size_t len);
 
 extern void
 libwebsocket_service_timeout_check(struct libwebsocket_context *context,
-				    struct libwebsocket *wsi, unsigned int sec);
+                    struct libwebsocket *wsi, unsigned int sec);
 
 extern struct libwebsocket * __libwebsocket_client_connect_2(
-	struct libwebsocket_context *context,
-	struct libwebsocket *wsi);
+    struct libwebsocket_context *context,
+    struct libwebsocket *wsi);
 
 extern struct libwebsocket *
 libwebsocket_create_new_server_wsi(struct libwebsocket_context *context);
 
 extern char *
 libwebsockets_generate_client_handshake(struct libwebsocket_context *context,
-		struct libwebsocket *wsi, char *pkt);
+        struct libwebsocket *wsi, char *pkt);
 
 extern int
 lws_handle_POLLOUT_event(struct libwebsocket_context *context,
-			      struct libwebsocket *wsi, struct pollfd *pollfd);
+                  struct libwebsocket *wsi, struct pollfd *pollfd);
 
 extern int
 lws_any_extension_handled(struct libwebsocket_context *context,
-						       struct libwebsocket *wsi,
-						       enum libwebsocket_extension_callback_reasons r,
-						       void *v, size_t len);
+                               struct libwebsocket *wsi,
+                               enum libwebsocket_extension_callback_reasons r,
+                               void *v, size_t len);
 
 extern void *
 lws_get_extension_user_matching_ext(struct libwebsocket *wsi,
-							struct libwebsocket_extension * ext);
+                            struct libwebsocket_extension * ext);
 
 extern int
 lws_client_interpret_server_handshake(struct libwebsocket_context *context,
-		struct libwebsocket *wsi);
+        struct libwebsocket *wsi);
 
 extern int
 libwebsocket_rx_sm(struct libwebsocket *wsi, unsigned char c);
 
 extern int
 lws_issue_raw_ext_access(struct libwebsocket *wsi,
-						unsigned char *buf, size_t len);
+                        unsigned char *buf, size_t len);
 
 #ifndef LWS_OPENSSL_SUPPORT
 
