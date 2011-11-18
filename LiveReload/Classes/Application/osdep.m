6ea92be480a226d0aada46cb1e9c16a93d9b0ff9
Andrey Tarantsov <andreyvit@gmail.com>
2011-11-19 01:23:03 +0700

    Fix autorelease impl on OS X

--- LiveReload/Classes/Application/osdep.m
+++ LiveReload/Classes/Application/osdep.m
@@ -7,27 +7,25 @@
 
 //----------------------------------
 
-static int run_loop_depth = 0;
-void os_autorelease_run_loop_callback (CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info) {
-    switch (activity) {
-        case kCFRunLoopEntry:
-            ++run_loop_depth;
-            break;
-        case kCFRunLoopExit:
-            --run_loop_depth;
-            // fall-thru
-        case kCFRunLoopBeforeWaiting:
-            if (run_loop_depth == 1) {
-                autorelease_cleanup();
-            }
-            break;
-    }
+@interface AutoReleaseMarker : NSObject
+@end
+
+@implementation AutoReleaseMarker
+
+- (void)dealloc {
+    autorelease_cleanup();
+    [super dealloc];
+}
+
+@end
+
+void autorelease_pool_activate() {
+    [[[AutoReleaseMarker alloc] init] autorelease];
 }
 
 //----------------------------------
 
 void os_init() {
-    CFRunLoopObserverCreate(NULL, kCFRunLoopEntry | kCFRunLoopExit | kCFRunLoopBeforeWaiting, YES, 0, os_autorelease_run_loop_callback, NULL);
 }
 
 const char *os_bundled_resources_path() {
--- Shared/autorelease.c
+++ Shared/autorelease.c
@@ -4,6 +4,12 @@
 #include <assert.h>
 #include <stdlib.h>
 
+// #define DEBUG_AUTORELEASE
+
+#ifdef DEBUG_AUTORELEASE
+#include <stdio.h>
+#endif
+
 
 #define MAX_AUTORELEASE 10000
 
@@ -13,8 +19,22 @@ static void *pool[MAX_AUTORELEASE] = { NULL };
 static void **last = pool;
 static void **end = pool + MAX_AUTORELEASE;
 
+#ifdef __APPLE__
+extern void autorelease_pool_activate();
+#endif
+
 void *autorelease(void *ptr) {
     if (ptr) {
+#ifdef DEBUG_AUTORELEASE
+        if (!*last) {
+            fprintf(stderr, "===> Autorelease pool activated.\n");
+        }
+#endif
+#ifdef __APPLE__
+        if (!*last) {
+            autorelease_pool_activate();
+        }
+#endif
         assert(last < end);
         *++last = ptr;
     }
@@ -22,8 +42,14 @@ void *autorelease(void *ptr) {
 }
 
 void autorelease_cleanup() {
-//    while (*last) {
-//        free(*last);
-//        --last;
-//    }
+#ifdef DEBUG_AUTORELEASE
+    if (*last) {
+        fprintf(stderr, "===> Autorelease pool draining.\n");
+        fflush(stderr);
+    }
+#endif
+    while (*last) {
+        free(*last);
+        --last;
+    }
 }
