4ea010dff02ffd4d8996e96cf6d7f6613290849f
Andrey Tarantsov <andreyvit@gmail.com>
2011-11-20 14:14:01 +0700

    Delete a bunch of unused classes

--- LiveReload/Classes/Application/LiveReloadAppDelegate.h
+++ LiveReload/Classes/Application/LiveReloadAppDelegate.h
@@ -3,20 +3,16 @@
 
 @class StatusItemController;
 @class NewMainWindowController;
-@class PreferencesWindowController;
 
 @interface LiveReloadAppDelegate : NSObject <NSApplicationDelegate> {
     StatusItemController  *_statusItemController;
     NewMainWindowController  *_mainWindowController;
-    PreferencesWindowController *_preferencesWindowController;
 }
 
 @property(nonatomic, retain) StatusItemController *statusItemController;
 @property(nonatomic, retain) NewMainWindowController *mainWindowController;
-@property(nonatomic, retain) PreferencesWindowController *preferencesWindowController;
 
 - (IBAction)displayMainWindow:sender;
-- (IBAction)displayPreferencesWindow:sender;
 - (IBAction)toggleMainWindow:sender;
 
 - (void)addProjectsAtPaths:(NSArray *)paths;
