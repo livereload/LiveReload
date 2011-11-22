758a499f38abe296efd7749c561887508567d0c4
Andrey Tarantsov <andreyvit@gmail.com>
2011-11-22 08:28:42 +0700

    Whitespace fix in Windows code

--- Windows/osdep.c
+++ Windows/osdep.c
@@ -12,7 +12,7 @@ const char *os_bundled_resources_path() {
     wchar_t buf[MAX_PATH];
     DWORD rv = GetModuleFileNameW(GetModuleHandle(NULL), buf, sizeof(buf)/sizeof(buf[0]));
     assert(rv);
-    
+
     char utf[MAX_PATH * 3];
     rv = WideCharToMultiByte(CP_UTF8, 0, buf, -1, utf, sizeof(utf)/sizeof(utf[0]), NULL, NULL);
     assert(rv);
