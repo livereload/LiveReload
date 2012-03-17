9a9f6b656f637568228b2f7c807c9c72045f5fe8
Andrey Tarantsov <andreyvit@gmail.com>
2012-03-18 00:52:40 +0700

    Add fsmonitor submodule (again, after merging with node branch)

---
+++ LiveReload/Classes/Application/version.h
@@ -0,0 +1,14 @@
+//
+//  version.h
+//  LiveReload
+//
+//  Created by Andrey Tarantsov on 3/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef LiveReload_version_h
+#define LiveReload_version_h
+
+
+
+#endif
