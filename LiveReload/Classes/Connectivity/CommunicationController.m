7e1c9b15acc8d1f694f431ffea403e53ccaf11e7
Andrey Tarantsov <andreyvit@gmail.com>
2012-03-21 12:28:51 +0700

    Whitespace fix

--- LiveReload/Classes/Connectivity/CommunicationController.m
+++ LiveReload/Classes/Connectivity/CommunicationController.m
@@ -13,7 +13,7 @@ void comm_broadcast_reload_requests(reload_session_t *session) {
     Project *project = (Project *)session->project;
 
     NSLog(@"Broadcasting change in %@", project.path);
-    
+
     SGLIB_SORTED_LIST_MAP_ON_ELEMENTS(reload_request_t, session->first, request, next, {
         json_t *arg = json_object();
         json_object_set_new(arg, "path", json_string(request->path));
