14c49d6853a3265ee00c732d46dee70527d31dba
Andrey Tarantsov <andreyvit@gmail.com>
2012-03-22 11:45:48 +0700

    Commit errorneously ignored livereload-backend.js

---
+++ backend/bin/livereload-backend.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+process.title = "LiveReloadHelper";
+
+global.LR = require('../config/env').createEnvironment();
+
+if (process.argv.indexOf('--console') >= 0) {
+    Path = require('path');
+
+    LR.rpc.init(process, process.exit, { callbackTimeout: 60000, consoleDebuggingMode: true });
+
+    LR.app.api.init({
+        pluginFolders: [ Path.join(__dirname, "../../LiveReload/Compilers")],
+        preferencesFolder: process.env['TMPDIR']
+    }, function(err) {
+        if (err) throw err;
+    });
+} else {
+    LR.rpc.init(process, process.exit);
+}
+
