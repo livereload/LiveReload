1b0b3879e8f886386d4152e05df465aa28cc62bc
Andrey Tarantsov <andreyvit@gmail.com>
2012-04-02 03:09:28 +0700

    Fix Rake tasks to also compile IcedCoffeeScript sources

--- tasks/backend.rb
+++ tasks/backend.rb
@@ -4,7 +4,10 @@ BACKEND_DIR = 'backend'
 COFFEE_SRC = FileList["#{BACKEND_DIR}/{app,bin,config,lib,logs,test}/**/*.coffee"]
 COFFEE_DST = COFFEE_SRC.ext('js')
 
-CLOBBER.include COFFEE_DST
+ICED_SRC = FileList["#{BACKEND_DIR}/{app,bin,config,lib,logs,test}/**/*.iced"]
+ICED_DST = COFFEE_SRC.ext('js')
+
+CLOBBER.include COFFEE_DST + ICED_DST
 
 namespace :backend do
 
@@ -18,12 +21,21 @@ namespace :backend do
             puts
             exit
         end
+        unless (`iced --version` rescue '') =~ /IcedCoffeeScript version 1\./
+            puts
+            puts "Please install IcedCoffeeScript:"
+            puts
+            puts "  npm install iced-coffee-script -g"
+            puts
+            exit
+        end
         sh 'coffee', '-c', *COFFEE_SRC
+        sh 'iced',   '-c', *ICED_SRC
     end
 
     desc "Install all backend prerequisites"
     task :install do
-        sh 'npm', 'install', 'coffee-script', '-g'
+        sh 'npm', 'install', 'coffee-script', 'iced-coffee-script', '-g'
         Dir.chdir(BACKEND_DIR) do
             sh 'npm', 'install'
         end
@@ -34,7 +46,7 @@ namespace :backend do
 
     desc "Remove all compiled JavaScript files in the backend"
     task :clobber do
-        rm COFFEE_DST
+        rm COFFEE_DST + ICED_DST
     end
 
 end
