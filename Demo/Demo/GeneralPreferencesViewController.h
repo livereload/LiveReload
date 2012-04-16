080dd55df3bb0f6415369d6c020350234fb31cc8
Vadim Shpakovski <vadim@shpakovski.com>
2012-04-16 21:26:04 +0300

    Removes Demo project. It will be hosted in another repository that will have current repository as a git submodule.

--- Demo/Demo/GeneralPreferencesViewController.h
+++
@@ -1,8 +0,0 @@
-//
-// This is a sample General preference pane
-//
-
-#import "MASPreferencesViewController.h"
-
-@interface GeneralPreferencesViewController : NSViewController <MASPreferencesViewController>
-@end
