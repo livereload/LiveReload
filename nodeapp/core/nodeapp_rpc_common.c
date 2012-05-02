4239ad30e2ac3cde9e3aa04ebed0b22f90cbcad8
Andrey Tarantsov <andreyvit@gmail.com>
2012-05-02 07:59:41 +0700

    Fix a nasty buffer overrun bug in the native RPC handler

--- nodeapp/core/nodeapp_rpc_common.c
+++ nodeapp/core/nodeapp_rpc_common.c
@@ -58,16 +58,16 @@ static void nodeapp_rpc_received_line(char *line) {
 void nodeapp_rpc_received_raw(char *buf, int cb) {
     strncat(nodeapp_rpc_receive_buf, buf, cb);
 
-    char *start = buf;
+    char *start = nodeapp_rpc_receive_buf;
     char *end;
     while ((end = (char *)strchr(start, '\n')) != NULL) {
         *end = 0;
         nodeapp_invoke_on_main_thread((INVOKE_LATER_FUNC)nodeapp_rpc_received_line, strdup(start));
         start = end + 1;
     }
-    if (start > buf) {
+    if (start > nodeapp_rpc_receive_buf) {
         // strings overlap, so can't use strcpy
-        memmove(buf, start, strlen(start) + 1);
+        memmove(nodeapp_rpc_receive_buf, start, strlen(start) + 1);
     }
 }
 
