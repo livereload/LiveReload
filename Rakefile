b57eccb44deaf0448e2c23145f1cb84198327ac0
Andrey Tarantsov <andreyvit@gmail.com>
2012-06-06 15:29:20 +0700

    Extract site into a separate repo

--- Rakefile
+++ Rakefile
@@ -254,14 +254,6 @@ task :push do
 end
 
 
-namespace :site do
-  desc "Publish the web site"
-  task :publish do
-    sh 'rsync', '-avz', 'site/', 'andreyvit_livereload@ssh.phx.nearlyfreespeech.net:/home/public/'
-  end
-end
-
-
 
 ################################################################################
 # Routing
--- site/framework/framework.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require dirname(__FILE__) . '/utilities.php';
-require dirname(__FILE__) . '/database.php';
-require dirname(__FILE__) . '/render.php';
-require dirname(__FILE__) . '/model.php';
-require dirname(__FILE__) . '/auth.php';
-require dirname(__FILE__) . '/param.php';
-require dirname(__FILE__) . '/is_email.php';
