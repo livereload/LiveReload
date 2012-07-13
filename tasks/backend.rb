aaf8df103acc4188f11ec18b2655f29ea590ffed
Andrey Tarantsov <andrey@tarantsov.com>
2012-07-13 15:40:57 +0700

    Compile node_modules/dreamlog in 'rake backend:compile'

--- tasks/backend.rb
+++ tasks/backend.rb
@@ -1,26 +1,15 @@
 
 BACKEND_DIR = 'backend'
 
-COFFEE_SRC = FileList["#{BACKEND_DIR}/{app,bin,config,lib,logs,test}/**/*.coffee"]
-COFFEE_DST = COFFEE_SRC.ext('js')
+ICED_SRC = FileList["#{BACKEND_DIR}/{app,bin,config,lib,logs,test,node_modules/dreamlog}/**/*.{coffee,iced}"]
+ICED_DST = ICED_SRC.ext('js')
 
-ICED_SRC = FileList["#{BACKEND_DIR}/{app,bin,config,lib,logs,test}/**/*.iced"]
-ICED_DST = COFFEE_SRC.ext('js')
-
-CLOBBER.include COFFEE_DST + ICED_DST
+CLOBBER.include ICED_DST
 
 namespace :backend do
 
     desc "Compile all CoffeeScript sources into JavaScript"
     task :compile do
-        unless (`coffee --version` rescue '') =~ /CoffeeScript version 1\./
-            puts
-            puts "Please install CoffeeScript:"
-            puts
-            puts "  npm install coffee-script -g"
-            puts
-            exit
-        end
         unless (`iced --version` rescue '') =~ /IcedCoffeeScript version 1\./
             puts
             puts "Please install IcedCoffeeScript:"
@@ -29,7 +18,6 @@ namespace :backend do
             puts
             exit
         end
-        sh 'coffee', '-c', *COFFEE_SRC
         sh 'iced', '--runtime', 'inline', '-c', *ICED_SRC
     end
 
@@ -46,7 +34,7 @@ namespace :backend do
 
     desc "Remove all compiled JavaScript files in the backend"
     task :clobber do
-        rm COFFEE_DST + ICED_DST
+        rm ICED_DST
     end
 
     desc "Run tests"
