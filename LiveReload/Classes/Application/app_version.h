ad47b0023fbc7886dab9f602d59e971a7cd04e12
Andrey Tarantsov <andrey@tarantsov.com>
2012-07-29 11:06:10 +0700

    Version bump to 2.4.0

--- LiveReload/Classes/Application/app_version.h
+++ LiveReload/Classes/Application/app_version.h
@@ -1,7 +1,7 @@
 #ifndef LiveReload_version_h
 #define LiveReload_version_h
 
-#define LIVERELOAD_VERSION "2.3.21"
+#define LIVERELOAD_VERSION "2.4.0"
 #define NODEAPP_VERSION LIVERELOAD_VERSION
 
 #endif
