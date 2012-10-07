d1fa8d7c07140165cd47123df0d14fd8c90cad82
Timofey Vasenin <timofey.vasenin@gmail.com>
2012-10-07 14:12:24 +0700

    Add tray context menu

--- TrayIconController.cs
+++ TrayIconController.cs
@@ -15,15 +15,39 @@ namespace LiveReload
     {
         MainWindow window;
         private System.Windows.Forms.NotifyIcon myNotifyIcon;
+        private ContextMenu contextMenuTray = new ContextMenu();
         
         public TrayIconController(MainWindow window_)
         {
             window = window_;
+
+            MenuItem menuItemRestore = new MenuItem("&Restore");
+            MenuItem menuItemExit    = new MenuItem("E&xit");
+
+            menuItemRestore.Click += new EventHandler(menuItemRestore_Click);
+            menuItemExit.Click    += new EventHandler(menuItemExit_Click);
+
+            contextMenuTray.MenuItems.Add(menuItemRestore);
+            contextMenuTray.MenuItems.Add(menuItemExit);
+            
             myNotifyIcon = new System.Windows.Forms.NotifyIcon();
             myNotifyIcon.Icon = new System.Drawing.Icon(@"img/LiveReload.ico");
             myNotifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(MyNotifyIcon_MouseClick);
+            myNotifyIcon.ContextMenu = contextMenuTray;
             myNotifyIcon.Visible = true;
         }
+
+        void menuItemRestore_Click(object sender, EventArgs e)
+        {
+            window.WindowState = WindowState.Normal;
+            window.ShowInTaskbar = true;
+        }
+
+        void menuItemExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
 
         private void MyNotifyIcon_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
         {
