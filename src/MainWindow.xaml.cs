2fbf3c84b920080269c33bfcde5fe6a6d735fa0d
Timofey Vasenin <timofey.vasenin@gmail.com>
2012-10-13 21:07:22 +0700

    Use MahApps.Metro in MainWindow styling

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace LiveReload
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
     {
         private List<ProjectData> projectsList;
 
