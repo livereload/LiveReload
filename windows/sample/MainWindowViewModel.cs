4b4bd77083705aeef5f944bad7d88f69e57ff658
Andrey Tarantsov <andrey@tarantsov.com>
2013-03-16 03:01:03 +0700

    Projects tree view styling WIP

---
+++ windows/sample/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiveReload
+{
+    public class ProjectViewModel
+    {
+        public string Name {
+            get {
+                return "";
+            }
+        }
+    }
+
+    public class MainWindowViewModel
+    {
+        public List<ProjectViewModel> SampleItems {
+            get {
+                return new ProjectViewModel[0].ToList();
+            }
+        }
+    }
+}
