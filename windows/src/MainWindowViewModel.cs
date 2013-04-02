5287a802345a7007840118dea0d5cfa1060770bd
Andrey Tarantsov <andrey@tarantsov.com>
2013-04-02 17:29:28 +0700

    Oops :-)

--- windows/src/MainWindowViewModel.cs
+++ windows/src/MainWindowViewModel.cs
@@ -27,7 +27,7 @@ namespace LiveReload
         public MainWindowViewModel(bool live) {
         }
 
-        public ActionsFilesViewModel ActionsFiles {Z:\livereload\devel\windows\src\MainWindowViewModel.cs
+        public ActionsFilesViewModel ActionsFiles {
             get {
                 return actionsFiles;
             }
