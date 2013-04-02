4f039a23d816bc2d7f155f070f9bce48f0ce4d7d
Andrey Tarantsov <andrey@tarantsov.com>
2013-04-02 21:51:34 +0700

    twins: Debugger persistence and UI improvements

--- windows/Twins/WPF/TwinsDebugger.xaml.cs
+++ windows/Twins/WPF/TwinsDebugger.xaml.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace Twins.WPF
@@ -16,14 +15,21 @@ namespace Twins.WPF
     public partial class TwinsDebugger : Window, ITwinsDebugger
     {
         private readonly TwinsEnvironment environment;
+        private readonly IsolatedStoragePreferenceStore preferenceStore;
+        private Timer timer;
 
         public TwinsDebugger(TwinsEnvironment environment) {
             this.environment = environment;
 
             InitializeComponent();
-            
+
             DataContext = new TwinsDebuggerViewModel(environment);
             ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+
+            preferenceStore = new IsolatedStoragePreferenceStore(IsolatedStorageFile.GetUserStoreForAssembly(), "TwinsDebuggerPreference.json");
+            ViewModel.Payload = (preferenceStore.Get("Payload") as string) ?? "";
+            ViewModel.PayloadHistoryMemento = (preferenceStore.Get("PayloadHistory") as string) ?? "";
+            new WindowLocationPersistence(this, preferenceStore, "DebuggerWindowLocation");
         }
 
         public TwinsDebuggerViewModel ViewModel {
@@ -33,10 +39,33 @@ namespace Twins.WPF
         }
 
         private void SendPayload_Click(object sender, RoutedEventArgs e) {
-            environment.SimulateRaw(ViewModel.Payload);
+            var payload = ViewModel.Payload;
+            environment.SimulateRaw(payload);
+            ViewModel.AddToHistory(payload);
+
+            SendConfirmationMark.Visibility = Visibility.Visible;
+
+            if (timer != null)
+                timer.Dispose();
+            timer = new Timer((state) => Application.Current.Dispatcher.Invoke(new Action(HideCheckmark)), null, TimeSpan.FromMilliseconds(250), TimeSpan.FromMilliseconds(-1));
+        }
+
+        private void HideCheckmark() {
+            SendConfirmationMark.Visibility = Visibility.Hidden;
+            timer.Dispose();
+            timer = null;
+        }
+
+        private void ClearPayload_Click(object sender, RoutedEventArgs e) {
+            ViewModel.Payload = "";
         }
 
         void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+            if (e.PropertyName == "Payload") {
+                preferenceStore.Update("Payload", ViewModel.Payload);
+            } else if (e.PropertyName == "PayloadHistory") {
+                preferenceStore.Update("PayloadHistory", ViewModel.PayloadHistoryMemento);
+            }
         }
 
         public void ShowDebugger() {
@@ -47,11 +76,21 @@ namespace Twins.WPF
             e.Cancel = true;
             Hide();
         }
+
+        private void PayloadHistoryCombo_SelectionChanged(object sender, RoutedEventArgs e) {
+            var combo = (ComboBox)sender;
+            var payload = combo.SelectedItem as string;
+            if (payload != null) {
+                ViewModel.Payload = payload;
+            }
+        }
     }
 
     public class TwinsDebuggerViewModel : ModelBase
     {
         private string payload;
+        private List<string> payloadHistory;
+        private const int historySize = 20;
 
         // design-time only
         public TwinsDebuggerViewModel() {
@@ -70,6 +109,32 @@ namespace Twins.WPF
                     payload = value;
                     OnPropertyChanged("Payload");
                 }
+            }
+        }
+
+        public List<string> PayloadHistory {
+            get {
+                return payloadHistory;
+            }
+        }
+
+        public void AddToHistory(string payload) {
+            while (payloadHistory.Remove(payload))
+                continue;
+            payloadHistory.Insert(0, payload);
+            if (payloadHistory.Count > historySize) {
+                payloadHistory.RemoveRange(historySize, payloadHistory.Count - historySize);
+            }
+            OnPropertyChanged("PayloadHistory");
+        }
+
+        public string PayloadHistoryMemento {
+            get {
+                return string.Join("\n~~~~~\n", payloadHistory);
+            }
+            set {
+                payloadHistory = value.Split(new string[] { "\n~~~~~\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                OnPropertyChanged("PayloadHistory");
             }
         }
     }
---
+++ windows/Twins/WPF/WindowLocationPersistence.cs
@@ -0,0 +1,142 @@
+﻿using LiveReload.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using Twins.JSON;
+
+namespace Twins.WPF
+{
+    public interface IPreferenceStore
+    {
+        object Get(string key, object defaultValue = null);
+        void Update(string key, object value);
+    }
+
+    public class IsolatedStoragePreferenceStore : IPreferenceStore
+    {
+        private readonly IsolatedStorageFile storage;
+        private readonly string fileName;
+        private readonly Encoding UTF8WithoutBOM = new UTF8Encoding(false);
+
+        public IsolatedStoragePreferenceStore(IsolatedStorageFile storage, string fileName) {
+            this.storage = storage;
+            this.fileName = fileName;
+        }
+
+        public object Get(string key, object defaultValue = null) {
+            var dictionary = ReadAll() as Dictionary<string, object>;
+            object result = defaultValue;
+            if (dictionary != null) {
+                dictionary.TryGetValue(key, out result);
+            }
+            return result;
+        }
+
+        public void Update(string key, object value) {
+            var dictionary = ReadAll() as Dictionary<string, object>;
+            if (dictionary == null) {
+                dictionary = new Dictionary<string, object>();
+            }
+            dictionary[key] = value;
+            WriteAll(dictionary);
+        }
+
+        private object ReadAll() {
+            try {
+                using (var reader = new StreamReader(new IsolatedStorageFileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read, storage), UTF8WithoutBOM)) {
+                    var text = reader.ReadToEnd();
+                    return Json.Parse(text);
+                }
+            } catch (Exception) {
+                return null;
+            }
+        }
+
+        private void WriteAll(object data) {
+            try {
+                var text = Json.Stringify(data, true);
+                using (var writer = new StreamWriter(new IsolatedStorageFileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None, storage), UTF8WithoutBOM)) {
+                    writer.WriteLine(text);
+                }
+            } catch (Exception e) {
+                System.Diagnostics.Debug.WriteLine("Error saving preferences to isolated storage file '{0}': {1} {2}", fileName, e.GetType().FullName, e.Message);
+            }
+        }
+    }
+
+    public class WindowLocationPersistence
+    {
+        private readonly Window window;
+        private readonly IPreferenceStore preferenceStore;
+        private readonly string preferenceKey;
+
+        public WindowLocationPersistence(Window window, IPreferenceStore preferenceStore, string preferenceKey) {
+            this.window = window;
+            this.preferenceStore = preferenceStore;
+            this.preferenceKey = preferenceKey;
+
+            window.LocationChanged += Window_SizeOrLocationChanged;
+            window.SizeChanged += Window_SizeOrLocationChanged;
+            window.StateChanged += Window_SizeOrLocationChanged;
+
+            Load();
+        }
+
+        public string Memento {
+            get {
+                Rect rect = new Rect(window.Left, window.Top, window.Width, window.Height);
+                if (window.WindowState != WindowState.Normal)
+                    rect = window.RestoreBounds;
+                return window.WindowState.ToString() + " " + rect.Left + " " + rect.Top + " " + rect.Width + " " + rect.Height;
+            }
+            set {
+                if (string.IsNullOrWhiteSpace(value))
+                    return;
+
+                var components = value.Split(' ');
+                if (components.Length < 5)
+                    throw new ArgumentException("Invalid memento");
+
+                Rect rect = new Rect(Convert.ToDouble(components[1]), Convert.ToDouble(components[2]), Convert.ToDouble(components[3]), Convert.ToDouble(components[4]));
+
+                var winFormsRect = new System.Drawing.Rectangle((int)rect.Left, (int)rect.Top, (int)rect.Width, (int)rect.Height);
+                var screen = Screen.FromRectangle(winFormsRect);
+                if (screen.WorkingArea.Contains(winFormsRect)) {
+                    // ideally we want to restore RestoreBounds when WindowState isn't Normal, but that property is read-only
+                    window.Left = rect.Left;
+                    window.Top = rect.Top;
+                    window.Width = rect.Width;
+                    window.Height = rect.Height;
+                }
+                
+                if (components[0] == "Normal")
+                    window.WindowState = WindowState.Normal;
+                else if (components[0] == "Maximized")
+                    window.WindowState = WindowState.Maximized;
+                else if (components[0] == "Minimized")
+                    window.WindowState = WindowState.Minimized;
+                else
+                    throw new ArgumentException("Invalid memento");
+            }
+        }
+
+        private void Load() {
+            var memento = preferenceStore.Get(preferenceKey) as string;
+            if (memento != null)
+                Memento = memento;
+        }
+
+        private void Save() {
+            preferenceStore.Update(preferenceKey, Memento);
+        }
+
+        void Window_SizeOrLocationChanged(object sender, EventArgs e) {
+            Save();
+        }
+    }
+}
