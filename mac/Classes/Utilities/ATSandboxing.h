c44d5d38114e0415fc7a898d37dda595ebc53662
Andrey Tarantsov <andrey@tarantsov.com>
2013-05-02 07:45:44 +0700

    Add sandboxing util from ex-3.x branch

---
+++ mac/Classes/Utilities/ATSandboxing.h
@@ -0,0 +1,17 @@
+
+#import <Foundation/Foundation.h>
+
+NSString *ATRealHomeDirectory();
+
+NSString *ATUserScriptsDirectory();
+
+BOOL ATIsSandboxed();
+BOOL ATAreSecurityScopedBookmarksSupported();
+BOOL ATIsUserScriptsFolderSupported();
+
+
+NSString *ATOSVersionString();
+int ATVersionMake(int major, int minor, int revision);
+int ATOSVersion();
+BOOL ATOSVersionAtLeast(int major, int minor, int revision);
+BOOL ATOSVersionLessThan(int major, int minor, int revision);
