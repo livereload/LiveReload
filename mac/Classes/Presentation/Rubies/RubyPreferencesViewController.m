069941bed4acb90c8b525050a372ceb506eeb6d9
Andrey Tarantsov <andrey@tarantsov.com>
2013-05-20 07:35:16 +0700

    Show a list of rubies on the pref page

--- mac/Classes/Presentation/Rubies/RubyPreferencesViewController.m
+++ mac/Classes/Presentation/Rubies/RubyPreferencesViewController.m
@@ -1,11 +1,20 @@
 
 #import "RubyPreferencesViewController.h"
 #import "AddCustomRubySheet.h"
+#import "RuntimeObject.h"
+#import "RubyManager.h"
+#import "RuntimeInstance.h"
+#import "RuntimeContainer.h"
 
 
-@interface RubyPreferencesViewController ()
+@interface RubyPreferencesViewController () <NSOutlineViewDataSource, NSOutlineViewDelegate>
+
+@property (assign) IBOutlet NSOutlineView *outlineView;
 
 @property(nonatomic, strong) NSWindowController *modalSheetController;
+
+@property(nonatomic, strong) RuntimeManager *repository;
+@property(nonatomic, strong) NSArray *topLevelItems;
 
 @end
 
@@ -15,8 +24,17 @@
 - (id)init {
     self = [super initWithNibName:NSStringFromClass([self class]) bundle:nil];
     if (self) {
+        self.repository = [RubyManager sharedRubyManager];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(runtimeRepositoryDidChange:) name:LRRuntimesDidChangeNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(runtimeRepositoryDidChange:) name:LRRuntimeContainerDidChangeNotification object:nil];
     }
     return self;
+}
+
+- (void)setView:(NSView *)view {
+    [super setView:view];
+
+    [self updateTopLevelItems];
 }
 
 - (NSString *)identifier {
@@ -44,5 +62,71 @@
 
     self.modalSheetController = nil;
 }
+
+
+
+#pragma mark - List of Instances and Containers
+
+- (void)updateTopLevelItems {
+    NSMutableArray *items = [NSMutableArray array];
+    [items addObjectsFromArray:self.repository.instances];
+    [items addObjectsFromArray:self.repository.containers];
+    self.topLevelItems = [NSArray arrayWithArray:items];
+    [self.outlineView reloadData];
+}
+
+- (void)runtimeRepositoryDidChange:(NSNotification *)notification {
+    [self updateTopLevelItems];
+}
+
+
+
+#pragma mark - NSTableView
+
+- (NSInteger)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(id)item {
+    if (item == nil)
+        return self.topLevelItems.count;
+    if ([item isKindOfClass:[RuntimeContainer class]])
+        return [[item instances] count];
+    return 0;
+}
+
+- (id)outlineView:(NSOutlineView *)outlineView child:(NSInteger)index ofItem:(id)item {
+    if (item == nil)
+        return self.topLevelItems[index];
+    if ([item isKindOfClass:[RuntimeContainer class]])
+        return [[item instances] objectAtIndex:index];
+    return nil;
+}
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item {
+    if (item == nil)
+        return YES;
+    if ([item isKindOfClass:[RuntimeContainer class]])
+        return YES;
+    return NO;
+}
+
+- (NSView *)outlineView:(NSOutlineView *)outlineView viewForTableColumn:(NSTableColumn *)tableColumn item:(id)item {
+
+    if ([item isKindOfClass:[RuntimeInstance class]]) {
+        RuntimeInstance *instance = item;
+
+        NSTableCellView *view = [outlineView makeViewWithIdentifier:@"Main" owner:self];
+        view.textField.stringValue = instance.title;
+        return view;
+    }
+    
+    if ([item isKindOfClass:[RuntimeContainer class]]) {
+        RuntimeContainer *container = item;
+
+        NSTableCellView *view = [outlineView makeViewWithIdentifier:@"Main" owner:self];
+        view.textField.stringValue = container.title;
+        return view;
+    }
+
+    return nil;
+}
+
 
 @end
