838024d6d0aad70dc63822fb919bafc447476abf
Timofey Vasenin <timofey.vasenin@gmail.com>
2013-06-24 08:15:42 +0700

    Switch PathSpec to non-portable class lib

--- windows/PathSpec/Properties/AssemblyInfo.cs
+++ windows/PathSpec/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -14,7 +13,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74117dd8-1259-44ef-b424-65fbed8a5799")]
 
 // Version information for an assembly consists of the following four values:
 //
