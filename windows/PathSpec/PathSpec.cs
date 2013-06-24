a9112e23ae293f0bed95ab39b86dd96f9105522f
Timofey Vasenin <timofey.vasenin@gmail.com>
2013-06-24 09:34:33 +0700

    Parse path lists

--- windows/PathSpec/PathSpec.cs
+++ windows/PathSpec/PathSpec.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -8,7 +9,11 @@ namespace PathSpec
 {
     public abstract class Mask
     {
-        public abstract bool Matches(string filename, string path);
+        public abstract bool Matches(string[] components, string path);
+
+        public bool Matches(string filename, string path) {
+            return Matches(new string[] { filename }, path);
+        }
     }
 
     public class FixedFileMask : Mask
@@ -19,8 +24,12 @@ namespace PathSpec
             this.filename = filename;
         }
 
-        public override bool Matches(string filename, string path) {
-            return this.filename == filename;
+        public override bool Matches(string[] components, string path) {
+            foreach (var component in components) {
+                if (component == this.filename)
+                    return true;
+            }
+            return false;
         }
     }
 
@@ -28,13 +37,14 @@ namespace PathSpec
     {
         private readonly string mask;
         private readonly Regex regex;
-        
+
         public WildcardFileMask(string mask) {
             this.mask = mask;
             this.regex = CompileToRegex(mask);
         }
 
         private static Regex CompileToRegex(string mask) {
+            // Do we need to handle all valid *nix path characters?
             string pattern =
                  '^' +
                  Regex.Escape(mask.Replace("*", "__:STAR:__")
@@ -48,8 +58,54 @@ namespace PathSpec
         // Windows internal file matching rules are crap and are not emulated here
         // (eg try to match filename "foobar..." with "foobar.?.")
         // Also: http://blogs.msdn.com/b/oldnewthing/archive/2007/12/17/6785519.aspx
-        public override bool Matches(string filename, string path) {
-            return regex.IsMatch(filename);
+        public override bool Matches(string[] components, string path) {
+            foreach (var component in components) {
+                if (regex.IsMatch(component))
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    public class PathList
+    {
+        private readonly List<Mask> masks = new List<Mask>();
+
+        public void Include(Mask mask) {
+            masks.Add(mask); // do we need to check for dupes?
+        }
+
+        public bool Includes(string path) {
+            var components = PathUtils.SplitPathComponents(path);
+            foreach (var mask in masks) {
+                if (mask.Matches(components, path))
+                    return true;
+            }
+            return false;
+        }
+
+        public void AddLines(string masks) {
+            using (var reader = new System.IO.StringReader(masks)) {
+                string line;
+                while ((line = reader.ReadLine()) != null) {
+                    var hashPos = line.IndexOf('#');
+                    if (hashPos != -1) {
+                        line = line.Substring(0, hashPos);
+                    }
+                    line = line.Trim();
+                    if (line.Length == 0)
+                        continue;
+
+                    var mask = PathSpec.Parse(line);
+                    Include(mask);
+                }
+            }
+        }
+
+        public static PathList Parse(string masks) {
+            var list = new PathList();
+            list.AddLines(masks);
+            return list;
         }
     }
 
@@ -61,6 +117,14 @@ namespace PathSpec
             } else {
                 return new FixedFileMask(mask);
             }
+        }
+    }
+
+    public static class PathUtils
+    {
+        public static string[] SplitPathComponents(string path) {
+            var separators = new char[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar };
+            return path.Split(separators, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
--- windows/PathSpecTests/PathSpecTests.cs
+++ windows/PathSpecTests/PathSpecTests.cs
@@ -1,27 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using PathSpec;
+
 namespace PathSpecTests
 {
     [TestClass]
-    public class PathSpecTests
+    public class MaskTests
     {
         [TestMethod]
-        public void TestTrivialFileMask() {
+        public void Mask_Trivial() {
             var foo = PathSpec.PathSpec.Parse("foo.txt");
             Assert.IsTrue(foo.Matches("foo.txt", @"C:\foo\bar\lol\foo.txt"));
             Assert.IsFalse(foo.Matches("pizda.dat", @"C:\foo\bar\lol\pizda.dat"));
         }
 
         [TestMethod]
-        public void TestWildcardFileMask() {
+        public void Mask_Wildcard() {
             var foo = PathSpec.PathSpec.Parse("*.txt");
             Assert.IsTrue(foo.Matches("foo.txt", @"C:\foo\bar\lol\foo.txt"));
             Assert.IsFalse(foo.Matches("pizda.dat", @"C:\foo\bar\lol\pizda.dat"));
         }
 
         [TestMethod]
-        public void TestComplexFileMask() {
+        public void Mask_Complex() {
             var foo = PathSpec.PathSpec.Parse("?*hello?.txt");
             Assert.IsTrue(foo.Matches("o_hello!.txt", @"C:\foo\bar\lol\o_hello!.txt"));
             Assert.IsTrue(foo.Matches("_hello!.txt", @"C:\foo\bar\lol\_hello!.txt"));
@@ -32,14 +34,14 @@ namespace PathSpecTests
         }
 
         [TestMethod]
-        public void TestSpecialFileMask() {
+        public void Mask_Special() {
             var foo = PathSpec.PathSpec.Parse("b*ar?.txt*");
             Assert.IsTrue(foo.Matches("barf.txt.bat", @"C:\foo\bar\lol\barf.txt.bat"));
             Assert.IsTrue(foo.Matches("board.dart.txt......", @"board.dart.txt......"));
         }
 
         [TestMethod]
-        public void TestI18nFileMask() {
+        public void Mask_I18n() {
             var хуй = PathSpec.PathSpec.Parse("?*привет?.txt");
             Assert.IsTrue(хуй.Matches("о_привет!.txt", @"C:\хуй\bar\lol\о_привет!.txt"));
             Assert.IsTrue(хуй.Matches("_привет!.txt", @"C:\хуй\bar\lol\_привет!.txt"));
@@ -47,6 +49,40 @@ namespace PathSpecTests
             Assert.IsFalse(хуй.Matches("о_привет!.png", @"C:\хуй\bar\goatse.cx\о_привет!.png"));
             Assert.IsFalse(хуй.Matches("!о_привет_хуй.png", @"C:\хуй\bar\goatse.cx\!о_привет_хуй.png"));
             Assert.IsFalse(хуй.Matches("хуй_привет.txt", @"C:\хуй\bar\lol\хуй_привет.txt"));
+        }
+    }
+
+    [TestClass]
+    public class PathListTests
+    {
+        private const string include =  "*.html" + "\n" +
+                                        "*.htm" + "\n" +
+                                        "*.shtml" + "\n" +
+                                        "*.xhtml    # 1999 just called" + "\n" +
+                                        "" + "\n" +
+                                        "*.asp      # 1995 is on the line as well" + "\n" +
+                                        "n" + "\n" +
+                                        "# Not sure what good comes from monitoring these" + "\n";
+        [TestMethod]
+        public void PathList_Trivial() {
+            var foo = PathList.Parse(include);
+            Assert.IsTrue(foo.Includes(@"C:\foo\bar\lol\foo.htm"));
+            Assert.IsTrue(foo.Includes(@"C:\foo\bar\lol\bar.xhtml"));
+            Assert.IsTrue(foo.Includes(@"C:\foo\bar\lol\lol.asp\n"));
+            Assert.IsFalse(foo.Includes(@"C:\# Not sure what good comes from monitoring these"));
+            Assert.IsFalse(foo.Includes(@"C:"));
+            Assert.IsFalse(foo.Includes(@"C:\"));
+            Assert.IsFalse(foo.Includes(@"lol"));
+        }
+
+        [TestMethod]
+        public void PathList_MatchInTheMiddle() {
+            var foo = PathList.Parse(include);
+            Assert.IsTrue(foo.Includes(@"C:\goatse.asp\haha"));
+            Assert.IsFalse(foo.Includes(@"C:\goatse.aspx\haha"));
+            Assert.IsTrue(foo.Includes(@"~/goatse.asp/"));
+            Assert.IsTrue(foo.Includes(@"~/goatse.asp/haha"));
+            Assert.IsFalse(foo.Includes(@"C:\# Not sure what good comes from monitoring these/haha"));
         }
     }
 }
