63981ca9e1854f854e812371ae6a40906f232a4c
Timofey Vasenin <timofey.vasenin@gmail.com>
2013-06-24 07:12:55 +0700

    Initial PathSpec/PathSpecTests

---
+++ windows/PathSpec/PathSpec.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PathSpec
+{
+    public abstract class Mask
+    {
+        public abstract bool Matches(string filename, string path);
+    }
+
+    public class FixedFileMask : Mask
+    {
+        private readonly string filename;
+
+        public FixedFileMask(string filename) {
+            this.filename = filename;
+        }
+
+        public override bool Matches(string filename, string path) {
+            return this.filename == filename;
+        }
+    }
+
+    public class WildcardFileMask : Mask
+    {
+        private readonly string mask;
+        private readonly Regex regex;
+        
+        public WildcardFileMask(string mask) {
+            this.mask = mask;
+            this.regex = CompileToRegex(mask);
+        }
+
+        private static Regex CompileToRegex(string mask) {
+            string pattern =
+                 '^' +
+                 Regex.Escape(mask.Replace("*", "__:STAR:__")
+                                  .Replace("?", "__:QM:__"))
+                     .Replace("__:STAR:__", ".*")
+                     .Replace("__:QM:__", ".")
+                 + '$';
+            return new Regex(pattern, RegexOptions.IgnoreCase);
+        }
+
+        // Windows internal file matching rules are crap and are not emulated here
+        // (eg try to match filename "foobar..." with "foobar.?.")
+        // Also: http://blogs.msdn.com/b/oldnewthing/archive/2007/12/17/6785519.aspx
+        public override bool Matches(string filename, string path) {
+            return regex.IsMatch(filename);
+        }
+    }
+
+    public class PathSpec
+    {
+        public static Mask Parse(string mask) {
+            if (mask.Contains("*") || mask.Contains("?")) {
+                return new WildcardFileMask(mask);
+            } else {
+                return new FixedFileMask(mask);
+            }
+        }
+    }
+}
---
+++ windows/PathSpecTests/PathSpecTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PathSpecTests
+{
+    [TestClass]
+    public class PathSpecTests
+    {
+        [TestMethod]
+        public void TestTrivialFileMask() {
+            var foo = PathSpec.PathSpec.Parse("хуй.txt");
+            Assert.IsTrue(foo.Matches("хуй.txt", @"C:\foo\bar\lol\хуй.txt"));
+            Assert.IsFalse(foo.Matches("pizda.dat", @"C:\foo\bar\lol\pizda.dat"));
+        }
+
+        [TestMethod]
+        public void TestWildcardFileMask() {
+            var foo = PathSpec.PathSpec.Parse("*.txt");
+            Assert.IsTrue(foo.Matches("хуй.txt", @"C:\foo\bar\lol\хуй.txt"));
+            Assert.IsFalse(foo.Matches("pizda.dat", @"C:\foo\bar\lol\pizda.dat"));
+        }
+
+        [TestMethod]
+        public void TestComplexFileMask() {
+            var foo = PathSpec.PathSpec.Parse("?*привет?.txt");
+            Assert.IsTrue(foo.Matches("о_привет!.txt", @"C:\foo\bar\lol\о_привет!.txt"));
+            Assert.IsTrue(foo.Matches("_привет!.txt", @"C:\foo\bar\lol\_привет!.txt"));
+            Assert.IsFalse(foo.Matches("привет_хуй.txt", @"C:\foo\bar\lol\привет_хуй.txt"));
+            Assert.IsFalse(foo.Matches("о_привет!.png", @"C:\foo\bar\lgoatse.cx\о_привет!.png"));
+            Assert.IsFalse(foo.Matches("!о_привет_хуй.png", @"C:\foo\bar\goatse.cx\!о_привет_хуй.png"));
+            Assert.IsFalse(foo.Matches("хуй_привет.txt", @"C:\foo\bar\lol\хуй_привет.txt"));
+        }
+
+        [TestMethod]
+        public void TestSpecialFileMask() {
+            var foo = PathSpec.PathSpec.Parse("b*ar?.txt*");
+            Assert.IsTrue(foo.Matches("barf.txt.bat", @"C:\foo\bar\lol\barf.txt.bat"));
+            Assert.IsTrue(foo.Matches("board.dart.txt......", @"board.dart.txt......"));
+        }
+    }
+}
