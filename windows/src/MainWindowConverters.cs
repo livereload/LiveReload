ebd11868804a2016d925abedca158f091072b220
Timofey Vasenin <timofey.vasenin@gmail.com>
2013-06-27 03:56:23 +0700

    Don't display project panel when not needed

---
+++ windows/src/MainWindowConverters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+
+using LiveReload.Model;
+
+namespace LiveReload
+{
+    [ValueConversion(typeof(Project), typeof(Visibility))]
+    public class SelectedProjectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value == null)
+                return Visibility.Hidden;
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
