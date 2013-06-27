381307c220b16eff03d7fc243827a3e9bf30fbe5
Timofey Vasenin <timofey.vasenin@gmail.com>
2013-06-28 02:21:55 +0700

    Eliminate dev command prompt for more build types

--- windows/BuildOfflineBuild.cmd
+++ windows/BuildOfflineBuild.cmd
@@ -1,7 +1,11 @@
 set pub_folder=bin\Debug\app.publish\
 
+
+set dotnetver=v4.0.30319
+set msbuild=%systemroot%\Microsoft.NET\Framework\%dotnetver%\MSBuild.exe
+
 :: need to clean and rebuild because property overrides are embedded into the app at build stage
-msbuild /t:clean,rebuild,publish /p:InstallFrom=Disk /p:IsWebBootstrapper=false
+"%msbuild%" /t:clean,rebuild,publish /p:InstallFrom=Disk /p:IsWebBootstrapper=false
 ren bin\Debug\app.publish\setup.exe LiveReloadSetupOffline.exe
 
 
