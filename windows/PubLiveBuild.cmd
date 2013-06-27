afc42aefb312d219fece2828e9188e32d0d7cdf1
Timofey Vasenin <timofey.vasenin@gmail.com>
2013-06-28 00:55:37 +0700

    No need to build from dev command prompt anymore

--- windows/PubLiveBuild.cmd
+++ windows/PubLiveBuild.cmd
@@ -1,3 +1,6 @@
+set dotnetver=v4.0.30319
+set msbuild=%systemroot%\Microsoft.NET\Framework\%dotnetver%\MSBuild.exe
+
 :: need to clean and rebuild because property overrides are embedded into the app at build stage
-msbuild /t:clean,rebuild,publish
+"%msbuild%" /t:clean,rebuild,publish
 ren bin\Debug\app.publish\setup.exe LiveReloadSetup.exe
