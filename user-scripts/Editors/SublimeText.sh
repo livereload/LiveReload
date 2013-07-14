aa463d6cd85ca23b800b961a509338ea8fec9c22
Andrey Tarantsov <andrey@tarantsov.com>
2013-07-14 23:22:53 +0700

    Simply Sublime editor script structure

--- user-scripts/Editors/SublimeText.sh
+++ user-scripts/Editors/SublimeText.sh
@@ -10,47 +10,41 @@ app_path="/Applications/Sublime Text 2.app"
 subl_path="$app_path/Contents/SharedSupport/bin/subl"
 
 case "$1" in
-    --check)
-        if ! test -d "$app_path"; then
-            echo "result: not-found"
-            echo "'$app_path' does not exist."
-            exit 1
-        elif ! test -f "$subl_path"; then
-            echo "result: broken"
-            echo "'$subl_path' does not exist."
-            exit 1
-        else
-            echo "result: found"
-            echo "$app_path"
-            exit 0
-        fi;;
     --help | "")
         echo "Usage: SublimeText.sh --check"
         echo "   or: SublimeText.sh <file> [<line> [<column>]]"
         exit 9;;
 esac
 
-file="$1"
-line="$2"
-column="$3"
+if ! test -d "$app_path"; then
+    echo "result: not-found"
+    echo "message: '$app_path' does not exist."
+    exit 1
+fi
 
-if test -n "$column"; then
-    "$subl_path" "$file:$line:$column" || {
-        echo "result: failed"
-        exit 2
-    }
-else
-    if test -n "$line"; then
-        "$subl_path" "$file:$line" || {
-            echo "result: failed"
-            exit 2
-        }
-    else
-        "$subl_path" "$file" || {
-            echo "result: failed"
-            exit 2
-        }
+if ! test -f "$subl_path"; then
+    echo "result: broken"
+    echo "message: '$subl_path' does not exist."
+    exit 1
+fi
+
+if test "$1" == "--check"; then
+    echo "result: found"
+    echo "$app_path"
+    exit 0
+fi
+
+location="$1"
+if test -n "$2"; then
+    location="$location:$2"
+    if test -n "$3"; then
+        location="$location:$3"
     fi
 fi
 
-echo "result: ok"
+if "$subl_path" "$location"; then
+    echo "result: ok"
+else
+    echo "result: failed"
+    exit 2
+fi
