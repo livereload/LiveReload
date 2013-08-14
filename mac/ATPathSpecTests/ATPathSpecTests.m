8a3d29debcbeb8e1b1821f3708f6dc8dbf1b02a8
Andrey Tarantsov <andrey@tarantsov.com>
2013-08-15 00:38:53 +0700

    A pathetic attempt to add a test target

---
+++ mac/ATPathSpecTests/ATPathSpecTests.m
@@ -0,0 +1,14 @@
+
+#import <XCTest/XCTest.h>
+
+@interface ATPathSpecTests : XCTestCase
+
+@end
+
+@implementation ATPathSpecTests
+
+- (void)testExample {
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
