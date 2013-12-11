d868472395e90a3a39004bcbe758981c69f96c0f
Andrey Tarantsov <andrey@tarantsov.com>
2013-12-11 16:08:15 +0700

    Add XCTest skeleton

---
+++ mac/LiveReloadTests/LiveReloadTests.m
@@ -0,0 +1,30 @@
+//
+//  LiveReloadTests.m
+//  LiveReloadTests
+//
+//  Created by Andrey Tarantsov on 11.12.2013.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LiveReloadTests : XCTestCase
+
+@end
+
+@implementation LiveReloadTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+
+@end
