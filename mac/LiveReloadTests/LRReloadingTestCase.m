ae725024c6e8abfff2450969d3d249591012f2b5
Andrey Tarantsov <andrey@tarantsov.com>
2014-05-21 17:47:43 +0700

    Add tests for the reloading cases broken by 2.x → 3.x transition

--- mac/LiveReloadTests/LRReloadingTestCase.m
+++ mac/LiveReloadTests/LRReloadingTestCase.m
@@ -7,8 +7,24 @@
 
 @implementation LRReloadingTestCase
 
-- (void)testExample {
+- (void)testSimpleHtml {
     XCTAssertNil([self runProjectTestNamed:@"reloading/simple_html" options:LRTestOptionNone], @"Failed");
+}
+
+- (void)testSimpleCss {
+    XCTAssertNil([self runProjectTestNamed:@"reloading/simple_css" options:LRTestOptionNone], @"Failed");
+}
+
+- (void)testCompiledLess {
+    XCTAssertNil([self runProjectTestNamed:@"reloading/compiled_less" options:LRTestOptionNone], @"Failed");
+}
+
+- (void)testFakeLess {
+    XCTAssertNil([self runProjectTestNamed:@"reloading/fake_less" options:LRTestOptionNone], @"Failed");
+}
+
+- (void)testSpecialReloadAllStylesheets {
+    XCTAssertNil([self runProjectTestNamed:@"reloading/special_reload_all_stylesheets" options:LRTestOptionNone], @"Failed");
 }
 
 @end
