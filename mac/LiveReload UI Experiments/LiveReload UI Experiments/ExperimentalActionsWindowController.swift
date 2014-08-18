35d7defaece62b3b3f600acdd8afa6b2c9b1d406
Andrey Tarantsov <andrey@tarantsov.com>
2014-08-18 07:45:20 +0700

    More WIP on experimental UI

--- mac/LiveReload UI Experiments/LiveReload UI Experiments/ExperimentalActionsWindowController.swift
+++ mac/LiveReload UI Experiments/LiveReload UI Experiments/ExperimentalActionsWindowController.swift
@@ -18,6 +18,9 @@ class ExperimentalActionsWindowController: NSWindowController, NSTableViewDataSo
 
     var measuringCell: ActionCellView!
 
+    var additionalMode = false
+    var expandedMode = false
+
     override func windowDidLoad() {
         super.windowDidLoad()
 
@@ -26,22 +29,30 @@ class ExperimentalActionsWindowController: NSWindowController, NSTableViewDataSo
         var rule = Rule()
 
         rule = Rule()
-        rule.checkboxLabel = "Sharing"
-        rule.introNarrative = "Enable to share these build settings with other team members via Gruntfile.js."
-        rule.detailedNarrative = "Enable to share these build settings with other team members via Gruntfile.js."
+        rule.alwaysVisible = true
+        rule.checkboxLabel = "Team Sharing"
+        rule.introNarrative1 = "Enable to share these build settings with other team members via Gruntfile.js."
+        rule.summaryNarrative = "Build settings are synced with Gruntfile.js."
+        rule.additionalNarrative = "Enable to share these build settings with other team members via Gruntfile.js."
+        rule.docNarratives = []
         items.append(rule)
 
         rule = Rule()
         rule.enabled = true
+        rule.alwaysVisible = true
+        rule.analysisNarrative = "You have 4 Less files in src/ subfolder."
         rule.checkboxLabel = "Less"
-        rule.introNarrative = "A CSS pre-processor with a CSS-like syntax. It adds variables, mixins, functions and other techniques to make stylesheets more maintainable, themable and extendable."
-        rule.detailedNarrative = "Compile [*.less] from [less/] subfolder into [css/] subfolder.\nUse the [latest stable] version of LESS (1.4.6)"
+        rule.introNarrative1 = "A CSS pre-processor with a CSS-like syntax. It adds variables, mixins, functions and other techniques to make stylesheets more maintainable, themable and extendable."
+        rule.summaryNarrative = "Compile 4 files from [less/] into [css/]"
+        rule.additionalNarrative = "Using the [latest stable] version of LESS (1.4.6)"
         items.append(rule)
 
         rule = Rule()
+        rule.alwaysVisible = false
         rule.checkboxLabel = "Sass"
-        rule.introNarrative = "“the most mature, stable, and powerful professional grade CSS extension language in the world”"
-        rule.detailedNarrative = "Compile [*.sass and *.scss] from [sass/] subfolder into [css/] subfolder.\nYou can start using more libraries and LiveReload will detect them, but you can also [browse the supported libraries] and add them manually."
+        rule.introNarrative1 = "“the most mature, stable, and powerful professional grade CSS extension language in the world”"
+        rule.summaryNarrative = "Compile 7 files from [sass/] into [css/]."
+        rule.additionalNarrative = "Compile [*.sass and *.scss] from [sass/] subfolder into [css/] subfolder.\nYou can start using more libraries and LiveReload will detect them, but you can also [browse the supported libraries] and add them manually."
         items.append(rule)
 
         tableView.reloadData()
@@ -67,16 +78,18 @@ class ExperimentalActionsWindowController: NSWindowController, NSTableViewDataSo
         let rule = items[row]
         cell.rule = rule
 
-        if rule.checkboxLabel.isEmpty {
-            cell.checkbox.hidden = true
-            cell.checkbox.title = ""
-        } else {
-            cell.checkbox.hidden = false
-            cell.checkbox.title = rule.checkboxLabel
-        }
+        cell.checkbox.hidden = rule.checkboxLabel.isEmpty
         cell.checkbox.state = (rule.enabled ? NSOnState : NSOffState)
 
+        configureCellViewCheckboxStyle(cell)
         configureCellViewNarrative(cell)
+    }
+
+    func configureCellViewCheckboxStyle(cell: ActionCellView) {
+        let rule = cell.rule
+
+        let checkboxColor = (rule.enabled ? NSColor.controlTextColor() : NSColor.secondaryLabelColor())
+        cell.checkbox.attributedTitle = NSAttributedString(string: rule.checkboxLabel, attributes: [NSFontAttributeName: NSFont.systemFontOfSize(NSFont.systemFontSizeForControlSize(.RegularControlSize)), NSForegroundColorAttributeName: checkboxColor])
     }
 
     func configureCellViewNarrative(cell: ActionCellView) {
@@ -85,7 +98,7 @@ class ExperimentalActionsWindowController: NSWindowController, NSTableViewDataSo
         let paraStyle = NSMutableParagraphStyle()
         paraStyle.paragraphSpacing = 12
 
-        let narrative = (rule.enabled ? rule.detailedNarrative : rule.introNarrative)
+        let narrative = (rule.enabled ? (expandedMode ? rule.detailedNarrative : rule.summaryNarrative) : (additionalMode || expandedMode ? rule.expandedIntroNarrative : rule.introNarrative))
 
         let formattedString = NSMutableAttributedString(string: narrative, attributes: [NSParagraphStyleAttributeName: paraStyle])
         replaceTextWithHyperlinks(formattedString)
@@ -103,6 +116,7 @@ class ExperimentalActionsWindowController: NSWindowController, NSTableViewDataSo
 
     func replaceTextWithHyperlinks(ass: NSMutableAttributedString) {
         var scanner = NSScanner(string: ass.string)
+        scanner.charactersToBeSkipped = NSCharacterSet()
         let hyperlinkStartCharset = NSCharacterSet(charactersInString: "[")
         let hyperlinkEndCharset = NSCharacterSet(charactersInString: "]")
         while !scanner.atEnd {
@@ -113,10 +127,12 @@ class ExperimentalActionsWindowController: NSWindowController, NSTableViewDataSo
                 var hyperlinkText: NSString?
                 scanner.scanUpToCharactersFromSet(hyperlinkEndCharset, intoString: &hyperlinkText)
                 if scanner.scanString("]", intoString: nil) {
+                    println("Found hyperlink '\(hyperlinkText)")
                     let hyperlinkEnd = scanner.scanLocation
                     ass.deleteCharactersInRange(NSMakeRange(hyperlinkEnd - 1, 1))
                     ass.deleteCharactersInRange(NSMakeRange(hyperlinkStart, 1))
                     scanner = NSScanner(string: ass.string)
+                    scanner.charactersToBeSkipped = NSCharacterSet()
                     scanner.scanLocation = hyperlinkEnd - 2
                     let range = NSMakeRange(hyperlinkStart, hyperlinkEnd - 2 - hyperlinkStart)
                     ass.addAttributes([NSLinkAttributeName: NSURL(string: "http://localhost:5000/\(NSString(string: hyperlinkText!).stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding))"), NSUnderlineStyleAttributeName: NSUnderlineStyleSingle | NSUnderlinePatternDot, NSUnderlineColorAttributeName: NSColor.secondaryLabelColor()], range: range)
@@ -129,23 +145,75 @@ class ExperimentalActionsWindowController: NSWindowController, NSTableViewDataSo
         println("Got click on \(url)")
     }
 
-    func clickedCheckboxInCellView(sender: ActionCellView) {
-        sender.rule.enabled = !sender.rule.enabled
-        configureCellViewNarrative(sender)
-        let row = tableView.rowForView(sender)
+    func clickedCheckboxInCellView(cell: ActionCellView) {
+        cell.rule.enabled = !cell.rule.enabled
+        configureCellViewCheckboxStyle(cell)
+        configureCellViewNarrative(cell)
+        let row = tableView.rowForView(cell)
         if row >= 0 {
             tableView.noteHeightOfRowsWithIndexesChanged(NSIndexSet(index: row))
         }
     }
 
+    @IBAction func toggleAdditionMode(sender: AnyObject) {
+        additionalMode = !additionalMode
+        tableView.reloadData()
+    }
+
+    @IBAction func toggleExpandedMode(sender: AnyObject) {
+        expandedMode = !expandedMode
+        tableView.enumerateAvailableRowViewsUsingBlock { (rowView, row) in
+            let cell = rowView.viewAtColumn(0) as ActionCellView
+            self.configureCellViewNarrative(cell)
+        }
+        tableView.noteHeightOfRowsWithIndexesChanged(NSIndexSet(indexesInRange: NSMakeRange(0, tableView.numberOfRows)))
+    }
+
 }
+
+
+private func joinStr(delimiter: String, a: String, b: String) -> String {
+    if a.isEmpty {
+        return b
+    } else if b.isEmpty {
+        return a
+    } else {
+        return a + delimiter + b
+    }
+}
+
+private func joinStr(delimiter: String, a: String, b: String, c: String, more: String...) -> String {
+    var result = joinStr(delimiter, a, b)
+    result = joinStr(delimiter, result, c)
+    for d in more {
+        result = joinStr(delimiter, result, d)
+    }
+    return result
+}
+
 
 class Rule : NSObject {
 
     var enabled = false
+    var alwaysVisible = false
     var checkboxLabel: String = ""
-    var introNarrative: String = ""
-    var detailedNarrative: String = ""
+    var introNarrative1: String = ""
+    var analysisNarrative: String = ""
+    var docNarratives: [String] = ["[Docs]", "[Version History]", "[Examples]", "[Issues]"]
+    var summaryNarrative: String = ""
+    var additionalNarrative: String = ""
+
+    var introNarrative: String {
+        return introNarrative1
+    }
+
+    var expandedIntroNarrative: String {
+        return joinStr("\n", introNarrative1, analysisNarrative, join("   ", docNarratives))
+    }
+
+    var detailedNarrative: String {
+        return joinStr("\n", summaryNarrative, additionalNarrative)
+    }
 
 }
 
