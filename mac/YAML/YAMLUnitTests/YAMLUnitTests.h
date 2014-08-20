262816402d8035902f066dc2045eb43fe6e29b6a
Andrey Tarantsov <andrey@tarantsov.com>
2014-08-20 20:49:09 +0700

    [YAML] Import d47d2ae
    * 665f17a 2010-09-26 Init <mirek@me.com>
    * 5ba1f38 2010-09-26 .gitignore <mirek@me.com>
    * 3922947 2010-09-26 Readme <mirek@me.com>
    * 56cf9c4 2010-09-26 Xcode project <mirek@me.com>
    * fc69c3a 2010-09-26 Tests <mirek@me.com>
    * 64b11ae 2010-09-26 LibYAML 0.1.3 <mirek@me.com>
    * 270af2e 2010-09-26 Small update, large yaml file generator <mirek@me.com>
    * 483980f 2010-09-26 http://www.yaml.org/spec/1.2/spec.html fetcher <mirek@me.com>
    * 2a27fbf 2010-09-26 http://www.yaml.org/spec/1.2/spec.html examples <mirek@me.com>
    * 883af0c 2010-09-27 Ignore large yaml example file (generated) <mirek@me.com>
    * 93b4b6a 2010-09-27 Events update <mirek@me.com>
    * 5470002 2010-09-27 Set error macro <mirek@me.com>
    * 4157a3c 2010-09-27 YAML spec 1.2 example featcher <mirek@me.com>
    * e21e251 2010-09-27 Test update <mirek@me.com>
    * 0aa5168 2010-09-27 Examples <mirek@me.com>
    * de9660b 2010-09-27 Examples <mirek@me.com>
    * 020ee5e 2010-09-27 Ignore large yaml example in the correct dir <mirek@me.com>
    * ea66b25 2010-10-15 impement writeYAML:toStream:options:error implement dataFromYAML:options:error add kYAMLErrorInvalidYamlObject error add copyright notice <Stanly@Silverblaze.local>
    * 4146b5e 2010-10-15 add tests for write methods <Stanly@Silverblaze.local>
    * b21bb06 2010-10-15 Don't initialize symbol in header. <pornel@pornel.net>
    * 36c7e37 2010-10-15 Initialize root and objects to nil in case code jumps to error early. <pornel@pornel.net>
    * 56b2995 2010-10-15 change 'documents' in YAMLSerializationWithDocument to autorelease, thus no need for gotos <Stanly@Silverblaze.local>
    * f45517a 2010-10-15 make framework's header resolveable after #import <YAML/YAMLSerialization.h> <Stanly@Silverblaze.local>
    * 6f0715f 2010-10-16 fix encoding in output, use YAML_UTF8_ENCODING convert non-string scalars to string representation for output <Stanly@Silverblaze.local>
    * de7e631 2010-10-16 fix problem with children de-serialization <Stanly@Silverblaze.local>
    * 4e02f4c 2010-10-19 fix garbage in written string when appending received buffer <Stanly@Silverblaze.local>
    * 49c9a5e 2010-10-20 No need to check for nil when releasing objects (Issue #1) <mirek@me.com>
    * c18a78b 2010-10-20 Use macro when setting errors <mirek@me.com>
    * 6d4f114 2011-05-20 Fixed error that prevented tests from running and crashed lib in use. <mbenjaminsmith@gmail.com>
    * d308c43 2012-01-06 Xcode 4.2.1 and .gitignore update <mirek@demo-pc.livedrive.local>
    * 3ed016f 2012-01-06 yaml-0.1.4 <mirek@demo-pc.livedrive.local>
    * 54ec8ce 2012-01-06 Removed yaml-0.1.3, added 0.1.4 to the project <mirek@demo-pc.livedrive.local>
    * 114165d 2011-07-31 fixed bug where error was being checked incorrectly, making deserialization from file impossible if you passed in the error parameter Conflicts: <carlb@pobox.com>
    * ee00024 2011-07-28 explicit conversions to suppress warnings and clean up build output <carlb@pobox.com>
    * e36e253 2011-07-31 Unit Tests <carlb@pobox.com>
    * 4b38b4a 2013-09-15 Cleanup. <mirek@me.com>
    * 1777e6f 2013-09-15 Updated read me. <mirek@me.com>
    * 94c25bb 2013-09-15 Indent fixes. <mirek@me.com>
    * 238f212 2013-09-15 Tabs to spaces. <mirek@me.com>
    * d47d2ae 2013-09-15 Updated project settings as suggested by Xcode 5. Changed #include <config.h> to #import “config.h” (using import to include file only once and \“ quotes to search in user header paths). Updated test.m a bit. <mirek@me.com>

---
+++ mac/YAML/YAMLUnitTests/YAMLUnitTests.h
@@ -0,0 +1,13 @@
+//
+//  YAMLUnitTests.h
+//  YAMLUnitTests
+//
+//  Created by Carl Brown on 7/31/11.
+//  Copyright 2011 PDAgent, LLC. Released under MIT License.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface YAMLUnitTests : SenTestCase
+
+@end
