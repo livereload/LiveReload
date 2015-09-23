e59aff560f8e191cc7befd76efdbad561488e462
Andrey Tarantsov <andrey@tarantsov.com>
2015-09-23 15:53:50 +0600

    🚧 New project skeleton

---
+++ LiveReload/Source/Application/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LiveReload
+//
+//  Created by Andrey Tarantsov on 2015-09-22.
+//  Copyright © 2015 LiveReload. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
