dfeb8efc5f7bf1acc49cd4e3a4aee076a2c62f6a
Andrey Tarantsov <andrey@tarantsov.com>
2016-09-10 11:15:42 +0700

    Add Podfile and a dummy integration project

---
+++ LiveReload/LRExperiments/LRExperiments/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LRExperiments
+//
+//  Created by Andrey Tarantsov on 2015-10-23.
+//  Copyright © 2015 Andrey Tarantsov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
